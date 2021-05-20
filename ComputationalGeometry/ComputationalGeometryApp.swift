08b5fa9f00a615cff3d817bb09de685d2a5887f4
donmcbrien <don.mcbrien@icloud.com>
2021-05-20 19:15:09 +0100

    Initial Commit

---
+++ ComputationalGeometry/ComputationalGeometryApp.swift
@@ -0,0 +1,17 @@
+//
+//  ComputationalGeometryApp.swift
+//  ComputationalGeometry
+//
+//  Created by Don McBrien on 20/05/2021.
+//
+
+import SwiftUI
+
+@main
+struct ComputationalGeometryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
